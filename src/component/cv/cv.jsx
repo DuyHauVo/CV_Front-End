c583dbf1508d222e7c71c55adee900fca4afd876
DuyHauVo <[ voduyhau272@gmaail.com])>
2024-06-28 15:35:59 +0700

    upload source

---
+++ src/component/cv/cv.jsx
@@ -0,0 +1,211 @@
+function Cv(){
+    return(
+     <>
+      <div className='wrapper'>
+     <div className="container">
+       <div className="content-left">
+         <div>
+           <img src="https://scontent.fdad3-6.fna.fbcdn.net/v/t39.30808-6/448510601_1909189019530161_7770431781770535508_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeFy8Msdp3P6IPNEJn60pf17NtZfO1rJ-UM21l87Wsn5Q-ufiD0LA1WXywpj9avm0oy3leJ5L8FDShayLYsluH94&_nc_ohc=l3k8BpfeDhwQ7kNvgG-GIMI&_nc_ht=scontent.fdad3-6.fna&oh=00_AYDalpbUDCsZMejAl0ZlvLfZNFow6kX2mdVLJfVA981u9w&oe=66807209" alt="" />
+         </div>
+         <div className="name">
+           <p className='full_name'>
+             <strong>VÕ DUY HẬU</strong>
+           </p>
+           <p className="design">
+             <strong>FRONT-END DEVELOPER</strong>
+           </p>
+         </div>
+         <div className="profile">
+           <div className="profile-contact">
+                 <p><strong>THÔNG TIN CÁ NHÂN</strong></p>
+                 <div className="line"></div>
+           </div>
+           <div className="profile-content">
+                 <div className="phone">
+                 <i class="fa-solid fa-phone"></i>
+                 <p><span>035759350</span></p>
+                 </div>
+                 <div className="email">
+                 <i class="fa-solid fa-envelope"></i>
+                 <p><span>voduyhau272@gmail.com</span></p>
+                 </div>
+                 <div className="link">
+                 <i class="fa-solid fa-circle-exclamation"></i>
+                 <p><span><a href="https://www.facebook.com/duy.hau.2702 ">https://www.facebook.com/duy.hau.2702   </a></span></p>
+                 </div>
+                 <div className="adress">
+                 <i class="fa-solid fa-location-dot"></i>
+                 <p><span>Thanh Khê, Thành Phố Đà Nẵng</span></p>
+                 </div>
+           </div>
+         </div>
+         <div className="skill">
+         <div className="skill-contact">
+                 <p><strong>KĨ NĂNG</strong></p>
+                 <div className="line"></div>
+           </div>
+          <div className="skill-content">
+             <div className="skill-1">
+               <div className="skill-1-1">
+                   <p><span>THÔNG THẠO CÁC THƯ VIỆN LẬP TRÌNH</span></p>
+               </div>
+               <div className="skill-1-2">
+                   <p><span>+ NodeJS, FrameWork, Fetch API, Angular, Bootstrap, React.</span></p>
+               </div>
+             </div>
+             <div className="skill-2">
+               <div className="skill-1-1">
+                   <p><span>THÔNG THẠO CÁC NGÔN NGỮ LẬP TRÌNH</span></p>
+               </div>
+               <div className="skill-1-2">
+                   <p><span>+ JavaScript, HTML&CSS, TypeScript, SQL.</span></p>
+               </div>
+             </div>
+             <div className="skill-3">
+               <div className="skill-1-1">
+                   <p><span>LÀM VIỆC NHÓM</span></p>
+               </div>
+               <div className="skill-1-2">
+                   <p><span>+ Tập trung cao trong quá trình làm việc nhóm. <br />
+                           + Hỗ trợ thành viên cùng nhau hoàn thành dự án.</span></p>
+               </div>
+             </div>
+             <div className="skill-4">
+               <div className="skill-1-1">
+                   <p><span>TIN HỌC VĂN PHÒNG</span></p>
+               </div>
+               <div className="skill-1-2">
+                   <p><span>THUYẾT TRÌNH <br /> GIAO TIẾP </span></p>
+               </div>
+             </div>
+             
+          </div>
+         </div>
+         <div className="language">
+         <div className="language-contact">
+                 <p><strong>TIẾNG ANH</strong></p>
+                 <div className="line"></div>
+           </div>
+           <div className="language-content">
+             <p><span>Đang học khoá giao tiếp tại trung tâm anh ngữ Iris.</span></p>
+           </div>
+         </div>
+       </div>
+       <div className="content-right">
+         <div className="target">
+           <div className="target-title contact">
+             <p><span>MỤC TIÊU CHUNG</span></p>
+             <div className="line"></div>
+           </div>
+           <div className="target-content">
+           <p><span>Sinh viên năm 2 trường FPOLY Đà Nẵng, chuyên ngành fontend-developer. Mong muốn học thêm nhiều kiến thức và kĩ năng từ các developer khác, muốn được vận dụng kiến thức đã học vào thực tiễn.Trong tương lai sẽ phấn đấu đạt được thực tập sinh xuất sắc và có thể trở thành nhân viên chính thức để được tham gia vào các dự án lớn của công ty. </span></p>
+           </div>
+         </div>
+         <div className="project">
+           <div className="project-title contact">
+               <p><span>DỰ ÁN</span></p>
+               <div className="line"></div>
+           </div>
+           <div className="project-content">
+             <div className="project-name">
+               <div className="name-content">
+                 <p><span>- DỰ ÁN WEDSITE GIỎ HÀNG</span></p>
+                 <p><span className='span2'>29/5/2023-20/7/2023</span></p>
+               </div>
+             </div>
+             <div className="pro-content">
+               <p><span>
+                 + Mô tả công việc: Viết code, chạy trang web, chỉnh sửa, viết báo cáo. <br />
+                 + Nội dung: Sử dụng HTML & CSS3.</span></p>
+               <p><span>+Code sản phẩm: <a href="https://github.com/DuyHauVo/HTML-CSS3">https://github.com/DuyHauVo/HTML-CSS3</a></span>
+               </p>
+             </div>
+           </div>
+           <div className="project-content">
+             <div className="project-name">
+               <div className="name-content">
+                 <p><span>- DỰ ÁN WEBSITE BÁN HÀNG ONLINE.</span></p>
+                 <p><span className='span2'>21/8/2023-20/9/2023 </span></p>
+               </div>
+             </div>
+             <div className="pro-content">
+               <p><span>
+               + Mô tả công việc: Viết code, chạy trang web, chỉnh sửa, viết báo cáo. <br />
+               + Nội dung: Sử dụng các thư viện tạo ra chác chức năng cho user và admin</span></p>
+               <p><span>+Code sản phẩm: <a href="https://github.com/DuyHauVo/Project/tree/main/src">https://github.com/DuyHauVo/Project/tree/main/src</a></span>
+               </p>
+             </div>
+           </div>
+           <div className="project-content">
+             <div className="project-name">
+               <div className="name-content">
+                 <p><span>- DỰ ÁN WEDSITE GIỎ HÀNG</span></p>
+                 <p><span className='span2'>29/5/2023-20/7/2023</span></p>
+               </div>
+             </div>
+             <div className="pro-content">
+               <p><span>
+                 + Mô tả công việc: Viết code, chạy trang web, chỉnh sửa, viết báo cáo. <br />
+                 + Nội dung: Sử dụng HTML & CSS3.</span></p>
+               <p><span>+Code sản phẩm: <a href="https://github.com/DuyHauVo/HTML-CSS3">https://github.com/DuyHauVo/HTML-CSS3</a></span>
+               </p>
+             </div>
+           </div>
+           <div className="project-content">
+             <div className="project-name">
+               <div className="name-content">
+                 <p><span>- DỰ ÁN WEDSITE GIỎ HÀNG</span></p>
+                 <p><span className='span2'>29/5/2023-20/7/2023</span></p>
+               </div>
+             </div>
+             <div className="pro-content">
+               <p><span>
+                 + Mô tả công việc: Viết code, chạy trang web, chỉnh sửa, viết báo cáo. <br />
+                 + Nội dung: Sử dụng HTML & CSS3.</span></p>
+               <p><span>+Code sản phẩm: <a href="https://github.com/DuyHauVo/HTML-CSS3">https://github.com/DuyHauVo/HTML-CSS3</a></span>
+               </p>
+             </div>
+           </div>
+           <div className="project-content">
+             <div className="project-name">
+               <div className="name-content">
+                 <p><span>- DỰ ÁN WEDSITE GIỎ HÀNG</span></p>
+                 <p><span className='span2'>29/5/2023-20/7/2023</span></p>
+               </div>
+             </div>
+             <div className="pro-content">
+               <p><span>
+                 + Mô tả công việc: Viết code, chạy trang web, chỉnh sửa, viết báo cáo. <br />
+                 + Nội dung: Sử dụng HTML & CSS3.</span></p>
+               <p><span>+Code sản phẩm: <a href="https://github.com/DuyHauVo/HTML-CSS3">https://github.com/DuyHauVo/HTML-CSS3</a></span>
+               </p>
+             </div>
+           </div>
+         </div>  
+         <div className="education">
+           <div className="education-title contact">
+               <p><span>HỌC VẤN</span></p>
+               <div className="line"></div>
+           </div>
+           <div className="education-content">
+             <div className="education-name">
+               <div className="name-content">
+                 <p><span>LẬP TRÌNH WEBSITE</span></p>
+                 <p><span className='span2'>T8/2022 - Hiện tại </span></p>
+               </div>
+               <div className="shool">
+                 <p><span>FPT Polytechnic Đà nẵng</span></p>
+               </div>
+             </div>
+             <div className="edu-content">
+                 <p><span>Trong quá trình học 6 kì tại trường tương đương với 2 năm đã làm và hoàn thành 4 dự án trong quá trình học và 1 dự án cá nhân</span></p>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
+     </>
+    )
+   }
+   export default Cv
